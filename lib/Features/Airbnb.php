ad163fe7a58413bd034b0b812b5b5c98861e98ef
Ostico Domenico Lupinetti <ostico@gmail.com>
2018-06-05 15:29:40 +0200

    Added parten pid for append

--- lib/Features/Airbnb.php
+++ lib/Features/Airbnb.php
@@ -11,8 +11,10 @@ namespace Features;
 use API\V2\Json\ProjectUrls;
 use Features\Airbnb\Utils\Email\ConfirmedQuotationEmail;
 use Features\Airbnb\Utils\Email\ErrorQuotationEmail;
+use Features\Dqf\Utils\ProjectMetadata;
 use Klein\Klein;
 use \Features\Outsource\Traits\Translated as TranslatedTrait;
+use RedisHandler;
 
 class Airbnb extends BaseFeature {
 
@@ -20,20 +22,59 @@ class Airbnb extends BaseFeature {
 
     const FEATURE_CODE = "airbnb";
 
+    protected static $service_types = [ 'standard', 'priority' ];
+
+    const REFERENCE_QUOTE_METADATA_KEY = "refers_to_pid";
+
     public static $dependencies = [];
 
     public static function loadRoutes( Klein $klein ) {
         //route( '/job/[:id_job]/[:password]/sign_off', 'GET', 'Features\Airbnb\Controller\SignOffController', 'signedOffCallback' );
     }
 
-    public function afterTMAnalysisCloseProject( $project_id , $_analyzed_report) {
+    public function afterTMAnalysisCloseProject( $project_id, $_analyzed_report ) {
+
+        $metadataDao = new \Projects_MetadataDao();
+        $quote_pid_append = $metadataDao->get( $project_id, Airbnb::REFERENCE_QUOTE_METADATA_KEY )->value;
+
+        if( !empty( $quote_pid_append ) ){
+            $this->setExternalParentProjectId( $quote_pid_append );
+        }
+
         $this->setSuccessMailSender( new ConfirmedQuotationEmail( self::getPluginBasePath() . '/Features/Airbnb/View/Emails/confirmed_quotation.html' ) );
         $this->setFailureMailSender( new ErrorQuotationEmail( self::getPluginBasePath() . '/Features/Airbnb/View/Emails/error_quotation.html' ) );
         $this->requestProjectQuote( $project_id, $_analyzed_report );
     }
 
-    public function projectUrls( ProjectUrls $formatted ){
+    public function projectUrls( ProjectUrls $formatted ) {
         return $formatted;
     }
+
+    /**
+     * Add options to project metadata
+     *
+     * @param $metadata
+     * @param $__postInput
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function filterProjectMetadata( $metadata, $__postInput ) {
+
+        if ( isset( $__postInput[ Airbnb::REFERENCE_QUOTE_METADATA_KEY ] ) && !empty( $__postInput[ Airbnb::REFERENCE_QUOTE_METADATA_KEY ] ) ) {
+            if ( !is_numeric( $__postInput[ Airbnb::REFERENCE_QUOTE_METADATA_KEY ] ) ) {
+                throw new \Exception( "Quote PID '{$__postInput[ Airbnb::REFERENCE_QUOTE_METADATA_KEY ]}' is not allowed. Only numbers allowed." );
+            }
+            $metadata[ Airbnb::REFERENCE_QUOTE_METADATA_KEY ] = $__postInput[ Airbnb::REFERENCE_QUOTE_METADATA_KEY ];
+        }
+
+        return $metadata;
+    }
+
+    public function filterNewProjectInputFilters( $filter_args ) {
+        $filter_args[ Airbnb::REFERENCE_QUOTE_METADATA_KEY ] = [ 'filter' => FILTER_SANITIZE_NUMBER_INT ];
+        return $filter_args;
+    }
+
 
 }
