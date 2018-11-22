2f6ab9c9501394cb73d2d0cf309c1d1f0453134d
domenico <domenico@translated.net>
2018-11-22 19:16:10 +0100

    Improved Filter: variables detection

--- lib/Features/Airbnb/Utils/SubFiltering/Filters/Variables.php
+++ lib/Features/Airbnb/Utils/SubFiltering/Filters/Variables.php
@@ -26,8 +26,9 @@ class Variables extends AbstractHandler {
          * - %{\n}$spaces=2%{\n}
          * - %{{(text-align=center)}}
          * - %{vars}
+         * - [AIRBNB] Reminder: Reply to %{guest}’s inquiry. |||| [AIRBNB] Reminder: Reply to %{guest}’s inquiry.
          */
-        preg_match_all( '/%{{[^}]*?}}|(%{[^}]*?}).*?\1|(%{[^}]*?})/', $segment, $html, PREG_SET_ORDER );
+        preg_match_all( '/%{{[^}]*?}}|(%{[^}]*?})[^\|]+?\1|(%{[^}]*?})/', $segment, $html, PREG_SET_ORDER );
         foreach ( $html as $pos => $variable ) {
             //replace subsequent elements excluding already encoded
             $segment = preg_replace(
