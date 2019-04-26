37fc6c4b0302e09298254dd12549b64e55e713d9
domenico <domenico@translated.net>
2019-04-26 17:13:36 +0200

    Temporarily disabled variable lock

--- lib/Features/Airbnb.php
+++ lib/Features/Airbnb.php
@@ -270,12 +270,12 @@ class Airbnb extends BaseFeature {
     }
 
     public function fromLayer0ToLayer1( Pipeline $channel ) {
-        $channel->addAfter( new HtmlToPh(), new Variables() );
+//        $channel->addAfter( new HtmlToPh(), new Variables() ); //FIXME: temporarily disabled
         return $channel;
     }
 
     public function fromLayer0ToLayer2( Pipeline $channel ) {
-        $channel->addAfter( new HtmlToPhToLayer2(), new Variables() );
+//        $channel->addAfter( new HtmlToPhToLayer2(), new Variables() ); //FIXME: temporarily disabled
         return $channel;
     }
 
