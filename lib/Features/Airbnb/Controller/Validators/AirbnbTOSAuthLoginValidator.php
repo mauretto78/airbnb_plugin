9c7b804641461fd66ccfeffb7f4ea8e607785962
domenico <domenico@translated.net>
2019-06-25 11:25:24 +0200

    Added Custom Validation for segment_delivery

---
+++ lib/Features/Airbnb/Controller/Validators/AirbnbTOSAuthLoginValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+use API\V2\Exceptions\AuthenticationError;
+use API\V2\Validators\LoginValidator;
+
+/**
+ * Created by PhpStorm.
+ * @author ostico domenico@translated.net / ostico@gmail.com
+ * Date: 25/06/19
+ * Time: 10.57
+ *
+ */
+
+class AirbnbTOSAuthLoginValidator extends LoginValidator {
+
+    public function _validate() {
+
+        $user = $this->controller->getUser();
+        $this->controller->getRequest()->method();
+        if( empty( $user )){
+            if( $this->controller->getRequest()->method() === 'GET' ){
+                $controllerInstance = new CustomPage();
+                $controllerInstance->setTemplate( "401.html" );
+                $controllerInstance->setCode( 401 );
+                $controllerInstance->doAction();
+                die(); // do not complete klein response, set 401 header
+            } else {
+                throw new AuthenticationError( "Invalid Login. You must be logged in MateCat in order to access to the segment delivery.", 401 );
+            }
+        }
+
+    }
+
+}
